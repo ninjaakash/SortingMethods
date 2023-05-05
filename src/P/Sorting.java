aaaf1e468911075a3183abbd8c5122a70a4320d4
akash <aakashmarkad@gmail.com>
2023-05-05 17:38:30 +0530

    bubble added

--- src/P/Sorting.java
+++ src/P/Sorting.java
@@ -23,6 +23,21 @@ public class Sorting {
 			}
 		}
 	}
+	public static void bubbelSort(int[] arr)
+	{
+		for(int i=0;i<arr.length-1;i++)
+		{
+			for(int j=0;j<arr.length;j++)
+			{
+				if(arr[i]>arr[j])
+				{
+					arr[i]=arr[i]+arr[j];
+					arr[j]=arr[i]-arr[j];
+					arr[i]=arr[i]-arr[j];
+				}
+			}
+		}
+	}
 	public static void selectiontionSort(int[] arr)
 	{
 		for(int i=1;i<arr.length;i++)
